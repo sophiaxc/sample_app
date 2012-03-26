7885a5d2134ec4821c0400074d096a8ef2146952
Sophia Cui <sophiaxc@gmail.com>
2012-03-25 21:29:07 -0700

    Finsh user edit, update, index, and destroy actions.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,15 @@ module SessionsHelper
     current_user = user
   end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
   def signed_in?
     !current_user.nil?
   end
@@ -11,6 +20,10 @@ module SessionsHelper
   def sign_out
     current_user = nil
     cookies.delete(:remember_token)
+  end
+
+  def current_user?(user)
+    current_user == user
   end
 
   def current_user=(user)
@@ -22,8 +35,13 @@ module SessionsHelper
   end
 
   private
+
     def user_from_remember_token
       remember_token = cookies[:remember_token]
       User.find_by_remember_token(remember_token) unless remember_token.nil?
+    end
+
+    def clear_return_to
+      session.delete(:return_to)
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,7 @@ describe User do
 
   subject { @user }
 
+  it { should respond_to(:admin) }
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
@@ -29,6 +30,13 @@ describe User do
   it { should respond_to(:remember_token) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+
+    it { should be_admin }
+  end
 
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
